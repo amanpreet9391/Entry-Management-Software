b5185969d78e18f28a49ba5793d9f1a0dedaaf94
amanpreet9391 <amanpreet9391@gmail.com>
2019-11-28 05:27:22 +0530

    remove extra files

--- sms.py
+++
@@ -1,8 +0,0 @@
-#pip install twilio
-# We need to sign up on twilio. After that we will generate a twilio phone number
-#twilio phone number - +19384440616
-
-import os
-from twilio.rest import Client
-account_sid = os.environ("TWILIO_ACCOUNT_SID")
-auth_token = os.environ("TWILIO_AUTH_TOKEN")
